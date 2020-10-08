f5b075d0398a6ca8efd4820104bf2c65364d42ae
dyaksa <diasnour0395@gmail.com>
2020-10-08 09:02:07 +0700

    add server dtabase

--- helper/DB.js
+++ helper/DB.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "zpayment",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12369413",
+  password: "bGhJ94t6Lk",
+  database: "sql12369413",
 });
 
 module.exports = conn.promise();
