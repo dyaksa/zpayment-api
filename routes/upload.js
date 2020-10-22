a6f1c3578c3c333917da34b94d6c3032b85dfdfa
dyaksa <diasnour0395@gmail.com>
2020-10-22 08:58:37 +0700

    add post upload

--- routes/upload.js
+++ routes/upload.js
@@ -4,6 +4,6 @@ const uploadController = require("../controller/uploadController");
 const verifyJwtToken = require("../middleware/verifyJwtToken");
 const upload = require("../middleware/upload");
 
-route.patch("/image",[verifyJwtToken.verifyToken,upload.uploadImage], uploadController.uploadPhoto);
+route.post("/images",[verifyJwtToken.verifyToken,upload.uploadImage], uploadController.uploadPhoto);
 
 module.exports = route;
