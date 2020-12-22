fa60575d61250b9e7c37a066726b6715c352cc62
dyaksa <diasnour0395@gmail.com>
2020-12-22 10:45:49 +0700

    deleted callback midtrans

--- routes/charge.js
+++ routes/charge.js
@@ -4,6 +4,7 @@ const chargeController = require("../controller/cahargeController");
 const verifyJwtToken = require("../middleware/verifyJwtToken");
 
 route.post("/",[verifyJwtToken.verifyToken],chargeController.charge);
+route.patch("/accepted",[verifyJwtToken.verifyToken],chargeController.accepted);
 
 
 module.exports = route;
