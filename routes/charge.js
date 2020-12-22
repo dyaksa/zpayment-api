1b03819ca7d8920f6e9e3c3944ea379d6551a80b
dyaksa <diasnour0395@gmail.com>
2020-12-22 10:49:19 +0700

    add route accepted transaction topup

--- routes/charge.js
+++ routes/charge.js
@@ -4,6 +4,6 @@ const chargeController = require("../controller/cahargeController");
 const verifyJwtToken = require("../middleware/verifyJwtToken");
 
 route.post("/",[verifyJwtToken.verifyToken],chargeController.charge);
-
+route.patch("/accepted",[verifyJwtToken.verifyToken],chargeController.accepted);
 
 module.exports = route;
